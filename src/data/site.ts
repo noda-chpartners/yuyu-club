df3746a4a988aa6a2ecd0075d4292b8768c1e2ae
Mode-s <shota.umemoto7@gmail.com>
2026-09-02 17:54:06 +0900

    fix

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,8 @@
 export const site = {
   name: '遊結倶楽部',
   reading: 'ゆうゆうくらぶ',
-  phoneDisplay: '090-3547-3989',
-  phoneHref: 'tel:+819035473989',
+  emailDisplay: 'yuyukurabu26@gmail.com',
+  emailHref: 'mailto:yuyukurabu26@gmail.com',
   address: '茨城県鹿嶋市棚木2819-7',
   station: '鹿島臨海鉄道大洗鹿島線 長者ヶ浜潮騒はまなす公園前駅より徒歩7分',
   businessDays: '月曜日・金曜日・日曜日（第1・3・5）',
